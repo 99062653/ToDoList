157efec7e1c2fa363bce1a98744d016d9c68fc2e
Rick Huisman <70889425+99062653@users.noreply.github.com>
2022-04-04 09:26:55 +0200

    1.0 deploy

--- code/connection.php
+++ code/connection.php
@@ -1,8 +1,7 @@
 <?php
 error_reporting(E_ERROR | E_PARSE); //zet alle warnings uit :)
-$servername = "localhost:3307";
-$username = "root";
-$password = "";
+$servername = "free-tier13.aws-eu-central-1.cockroachlabs.cloud:26257";
+$username = "rick";
+$password = "-j7QkZdjLknQ3_A8HPwbhw";
 
-$conn = new mysqli($servername, $username, $password, "todolistdatabase");
-?>
+$conn = new mysqli($servername, $username, $password, "todolistdb-766.defaultdb");
