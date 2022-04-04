f7a93658ed69520cc093e4885efa1174c9cbf47b
Rick Huisman <70889425+99062653@users.noreply.github.com>
2022-04-04 14:32:59 +0200

    Update admin.php

--- pages/admin.php
+++ pages/admin.php
@@ -24,30 +24,31 @@ if (isset($_COOKIE["userid"])) {
         </div>
     </header>
     <div id="container">
-        <div id="header-container">
-            <h1><?php echo $_COOKIE["username"]; ?>, **ALLE** lijsten <span class="header-new"><a class="urls" href="pages/list.php?list=1&task=0&new=1">Nieuwe lijst + </a></span></h1>
-        </div>
-        <div id="lists">
-            <?php for ($i = 0; $i < count($listrows); $i++) { ?>
-                <div id="list">
-                    <h2><?= $listrows[$i]["title"] ?> <span class="list-edit"><a class="urls" href="pages/list.php?list=1&task=0&new=0&id=<?= $listrows[$i]["id"] ?>">Edit</a></span></h2>
-                    <p><?= strlen($listrows[$i]["description"]) > 0 ? $listrows[$i]["description"] : "Geen description" ?></p>
-                    <p>Created by <?= $listrows[$i]["madeby"] ?></p>
-                    <?php if (isset($listrows[$i]["tasks"])) { ?>
+        <?php if (isset($_COOKIE["userid"])) { ?>
+            <div id="header-container">
+                <h1><?php echo $_COOKIE["username"]; ?>, **ALLE** lijsten <span class="header-new"><a class="urls" href="pages/list.php?list=1&task=0&new=1">Nieuwe lijst + </a></span></h1>
+            </div>
+            <div id="lists">
+                <?php for ($i = 0; $i < count($listrows); $i++) { ?>
+                    <div id="list">
+                        <h2><?= $listrows[$i]["title"] ?> <span class="list-edit"><a class="urls" href="pages/list.php?list=1&task=0&new=0&id=<?= $listrows[$i]["id"] ?>">Edit</a></span></h2>
+                        <p><?= strlen($listrows[$i]["description"]) > 0 ? $listrows[$i]["description"] : "Geen description" ?></p>
+                        <p>Created by <?= $listrows[$i]["madeby"] ?></p>
+                        <?php if (isset($listrows[$i]["tasks"])) { ?>
                             <button class="urls task-sort" onclick="displaySort(<?= $listrows[$i]["id"] ?>)">Sort</button><button class="urls task-filter" onclick="displayFilter(<?= $listrows[$i]["id"] ?>)">Filter</button>
                         <?php } ?>
-                    <a class="urls task-add" href="pages/task.php?list=0&task=1&new=1&listid=<?php echo $listrows[$i]["id"] ?>">+</a>
-                    <form id="sort-<?= $listrows[$i]["id"] ?>" class="sorts" method="post">
-                        <input type="text" name="sortlistid" value="<?= $listrows[$i]["id"] ?>" hidden>
-                        <select name="sort-time">
-                            <option value="unset">Tijd</option>
-                            <option value=">"> > </option>
-                            <option value="<">
-                                < </option>
-                        </select>
-                        <input type="submit" value="Sorteer">
-                    </form>
-                    <form id="filter-<?= $listrows[$i]["id"] ?>" class="filters" method="post">
+                        <a class="urls task-add" href="pages/task.php?list=0&task=1&new=1&listid=<?php echo $listrows[$i]["id"] ?>">+</a>
+                        <form id="sort-<?= $listrows[$i]["id"] ?>" class="sorts" method="post">
+                            <input type="text" name="sortlistid" value="<?= $listrows[$i]["id"] ?>" hidden>
+                            <select name="sort-time">
+                                <option value="unset">Tijd</option>
+                                <option value=">"> > </option>
+                                <option value="<">
+                                    < </option>
+                            </select>
+                            <input type="submit" value="Sorteer">
+                        </form>
+                        <form id="filter-<?= $listrows[$i]["id"] ?>" class="filters" method="post">
                             <input type="text" name="filterlistid" value="<?= $listrows[$i]["id"] ?>" hidden>
                             <select name="filter-status">
                                 <option value="unset">Status</option>
@@ -58,18 +59,23 @@ if (isset($_COOKIE["userid"])) {
                             </select>
                             <input type="submit" value="Filter">
                         </form>
-                    <div id="tasks">
-                        <?php foreach ($listrows[$i]["tasks"] as $tasks) { ?>
-                            <div id="task">
-                                <h3><?= $tasks["description"] ?><span class="task-edit"><a class="urls" href="pages/task.php?list=0&task=1&new=0&id=<?= $tasks["id"] ?>">Edit</a></span></h3>
-                                <h3>Status: <?= $tasks["status"] ?></h3>
-                                <h3>Time: <?= $tasks["time"] ?></h3>
-                            </div>
-                        <?php } ?>
+                        <div id="tasks">
+                            <?php foreach ($listrows[$i]["tasks"] as $tasks) { ?>
+                                <div id="task">
+                                    <h3><?= $tasks["description"] ?><span class="task-edit"><a class="urls" href="pages/task.php?list=0&task=1&new=0&id=<?= $tasks["id"] ?>">Edit</a></span></h3>
+                                    <h3>Status: <?= $tasks["status"] ?></h3>
+                                    <h3>Time: <?= $tasks["time"] ?></h3>
+                                </div>
+                            <?php } ?>
+                        </div>
                     </div>
-                </div>
-            <?php } ?>
-        </div>
+                <?php } ?>
+            </div>
+        <?php } else { ?>
+            <div id="header-container">
+                <h1>Login of Sign up!</h1>
+            </div>
+        <?php } ?>
     </div>
     <footer>
         <a class="urls" href="https://github.com/99062653" target="_blank">Rick Huisman &copy; <?= date("Y"); ?></a>
